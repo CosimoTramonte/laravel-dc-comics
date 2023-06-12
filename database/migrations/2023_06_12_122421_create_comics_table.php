04c64e134c52e3c2df3c9394d3715f5f7b92fa4d
CosimoTramonte <cosimotramonte7@gmail.com>
2023-06-12 14:37:14 +0200

    add migration

--- database/migrations/2023_06_12_122421_create_comics_table.php
+++ database/migrations/2023_06_12_122421_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 5, 2);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
